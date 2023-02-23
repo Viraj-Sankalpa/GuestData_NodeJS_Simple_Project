0ada2379465351c443f55e05806e6a5ac6e0ef8e
viraj sankalpa <viraj@pop-os.localdomain>
2023-02-23 17:04:32 +0530

    guestDB.js debug

--- guestDB.js
+++ guestDB.js
@@ -2,4 +2,21 @@
 
 // console.log("guestDB.js");
 
-const name= "viraj";
+// const name= "viraj";
+// const age = 100;
+
+const add = ()=>{
+    return "Adding...";
+}
+const view = ()=>"View..";
+
+
+
+
+//if we want to use this name variable, we should export it
+// module.exports = name;
+
+module.exports = {
+    add,
+    view,
+}
