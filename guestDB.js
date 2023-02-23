7db4e0edc15c77bbe1aa57e22e468c1098ffb8e0
viraj sankalpa <viraj@pop-os.localdomain>
2023-02-24 00:04:08 +0530

    update user ok

--- guestDB.js
+++ guestDB.js
@@ -25,19 +25,68 @@ const addGuest=(name,address,contact,visit_date)=>{
 }
 
 const updateGuest=(id,name,address,contact,visit_date)=>{
-    console.log(chalk.yellow("Update",id));
+    const guests = loadGuest();
+    const guestIndex = guests.findIndex((guest)=>{
+        return guest.id === id;
+    });
+    // console.log(guestIndex);
+
+    if(guestIndex != -1){
+        const guest = guests[guestIndex];
+        // if(name){
+        //     console.log(name);
+        // }else{
+        //     console.log(chalk.red("No name"));
+        // }
+
+        guest.name = name ? name:guest.name;
+        guest.address = address ? name:guest.address;
+        guest.contact = contact ? contact:guest.contact;
+        guest.visit_date = visit_date ? visit_date:guest.visit_date;
+
+        console.log(chalk.yellow("Record Updated ",id));
+        saveGuest(guests);
+    }else{
+        console.log(chalk.red("No record found"));
+    }
+    
 }
 
 const deleteGuest=(id)=>{
+    const guests = loadGuest();
+    const newGuests= guests.filter((guest)=>{
+        return guest.id != id;
+    });
+
+    if(guests.length > newGuests.length){
+        saveGuest(newGuests);
     console.log(chalk.red("Delete",id));
+    }else{
+        console.log(chalk.red.inverse("Can't find the record"));
+    }
+
 }
 
 const readGuest=(id)=>{
-    console.log(chalk.blue("Read",id));
+    const guests = loadGuest();
+    const guest = guests.find((guest)=>{
+        return guest.id ==id;
+    });
+
+    if(guest){
+        console.log(chalk.blue.inverse(guest.name));
+    }else{
+        console.log(chalk.red.inverse("Can't find the user"));
+    }
 }
 
 const listGuest=()=>{
     console.log(chalk.magenta("List"));
+    const guests = loadGuest();
+    guests.forEach((guest)=>{
+        console.log(guest);
+    })
+
 }
 
 //saveGuest
