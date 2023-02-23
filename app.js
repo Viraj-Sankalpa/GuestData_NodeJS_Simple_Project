00f95768da2d77c3abc58959ed70e433d628e7e1
viraj sankalpa <viraj@pop-os.localdomain>
2023-02-23 17:03:54 +0530

    app.js debug

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
 //call guestDB.js
-require("./guestDB");
+const db = require("./guestDB");
 
 //get data from console
+// console.log("app.js");
 
-
-
-console.log("app.js");
+// console.log(name);
+console.log(db.add());
+console.log(db.view());
